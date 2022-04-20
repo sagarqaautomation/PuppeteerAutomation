de94befb80c4e6088b89607f325e8b9051ab6d00
sagar <sagarqaautomation@gmail.com>
2022-04-20 17:00:35 +0530

    inital commit

---
+++ Sample.js
@@ -0,0 +1,29 @@
+const puppeteer=require('puppeteer');
+
+let config ={
+
+	launchOptions:{
+		headless:false
+	}
+} 
+
+//Page Locaters
+const login={
+	email:'input[id="username"]',
+	password:'input[id="password"]'
+		}
+
+puppeteer.launch(config.launchOptions).then(async browser=> {
+ const page=await browser.newPage();
+ await page.goto('https://app.hubspot.com/login');
+ await page.waitFor(login.email);
+ await page.type(login.email,"puppeteer");
+ await page.type(login.password,"puppeteer");
+ await page.screenshot({path:'loginpage.png',fullPage:true})
+// if you want to generate pdf we need to run in headlessmode
+ //await page.pdf({path:'loginpage.pdf'})
+ //await browser.close();
+
+ 
+	
+});
